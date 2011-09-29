061b0709edefc81761494cee39aa7f4373855780
Christijan <ijantje@gmail.com>
2011-09-29 10:05:43 -0600

    added blog drag function

--- dragmatch.lua/main.lua
+++ dragmatch.lua/main.lua
@@ -1,1 +1,24 @@
-_W = 
+-- create object
+local myObject = display.newRect( 0, 0, 100, 100 )
+myObject:setFillColor( 255 )
+
+-- touch listener function
+function myObject:touch( event )
+    if event.phase == "began" then
+
+        self.markX = self.x    -- store x location of object
+        self.markY = self.y    -- store y location of object
+
+    elseif event.phase == "moved" then
+
+        local x = (event.x - event.xStart) + self.markX
+        local y = (event.y - event.yStart) + self.markY
+        
+        self.x, self.y = x, y    -- move object based on calculations above
+    end
+    
+    return true
+end
+
+-- make 'myObject' listen for touch events
+myObject:addEventListener( "touch", myObject )
