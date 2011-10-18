43f2fdce241af1c904eae6ba893e86da070f57c8
christijan <christijand@mediarain.com>
2011-10-17 22:34:45 -0600

    improve quiz quality, got http request to get images
    Made images not line up in order so it seems like more of a guessing
    game. Smallified images and got permissions installed so images are
    grabbed on Android devices.

--- dragmatch.lua/main.lua
+++ dragmatch.lua/main.lua
@@ -1,7 +1,11 @@
 _H = display.contentHeight
 _W = display.contentWidth
 
-xValue = _W/3.75
+-- Load the relevant LuaSocket modules
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+
+xValue = _W/3.8
 yValue = _H/6.2
 print("Y: "..yValue)
 
@@ -9,11 +13,25 @@ print("Y: "..yValue)
 
 
 local imageTable = {}
-imageTable[4] = "images/triceratops.jpg"
-imageTable[1] = "images/sabertoothLive.jpg"
-imageTable[2] = "images/trexLive.jpg"
-imageTable[3] = "images/raptorLive.jpg"
+imageTable[4] = "tritop.jpg"
+imageTable[1] = "stooth.jpg"
+imageTable[2] = "t-rex.jpg"
+imageTable[3] = "vraptor.jpg"
 
+for i = 1,4 do
+
+-- Create local file for saving data
+local path = system.pathForFile( imageTable[i], system.DocumentsDirectory )
+myFile = io.open( path, "w+b" ) 
+	-- Request remote file and save data to local file
+http.request{
+    url = "http://christijandraper.com/coronaImages/"..imageTable[i] ,
+    sink = ltn12.sink.file(myFile),
+}
+
+
+ 
+end
 
 local button = display.newRect(0,0,44,44)
 button.x = _W/2
@@ -23,29 +41,33 @@ dropTainer = {}
 for i = 1,4 do
 	-- Create four locations to drop photos
 
-	local dropContainer = display.newImageRect(imageTable[i],_W/2.75,_H/6)
+	local dropContainer = display.newImageRect(imageTable[i],system.DocumentsDirectory,_W/2.75,_H/6)
 	dropContainer.x = xValue
 	dropContainer.y = yValue
 	--dropContainer:setFillColor(0)
 	dropContainer.strokeWidth=1
 	dropContainer:setStrokeColor(250,250,250)
 	if (i == 1) or (i == 3) then
-		xValue = xValue *2.75
+		xValue = xValue *2.8
 	end
 	if (i == 2) then
-		xValue = xValue / 2.75
+		xValue = xValue / 2.8
 		yValue = yValue *2.1
 		
 	end
 	dropTainer[i] = dropContainer
+	dropTainer[i].ID = imageTable[i]
 end
 -- Create four photos to match each of the four titles
 
---xValue = _W/3.75
---yValue = yValue *2.1
-
 function dragPix( event )
-	local objectNum = event.target.j
+-- Find the right dropTainer
+	local objectNum
+	for num = 1,4 do
+		if (string.sub(dropTainer[num].ID,1,-5).."Skull" == string.sub(event.target.ID,1,-5)) then
+			objectNum = num
+		end
+	end
     if event.phase == "began" then
         event.target.markX = event.target.x    -- store x location of object
         event.target.markY = event.target.y    -- store y location of object
@@ -57,26 +79,43 @@ function dragPix( event )
 		
         event.target.x, event.target.y = x, y    -- move object based on calculations above
 
--- Snap to correct location		
-		if (x/dropTainer[objectNum].x > .8) and (y/dropTainer[objectNum].y > .8) 
-			and (x/dropTainer[objectNum].x < 1.2) and (y/dropTainer[objectNum].y < 1.2) then
+-- Snap to correct location
+		if (x/dropTainer[objectNum].x > .7) and (y/dropTainer[objectNum].y > .7) 
+			and (x/dropTainer[objectNum].x < 1.3) and (y/dropTainer[objectNum].y < 1.3) then
 			event.target.x = dropTainer[objectNum].x
 			event.target.y = dropTainer[objectNum].y
+			
+			event.target:removeEventListener("touch",dragPix)
+			system.vibrate()
 			display.getCurrentStage():setFocus(nil)
 		end
 	elseif event.phase == "ended" and event.target.x ~= dropTainer[objectNum].x and event.target.y ~= dropTainer[objectNum].y then
 			event.target.x = event.target.markX
 			event.target.y = event.target.markY
 			display.getCurrentStage():setFocus(nil)
+	
     end
   return true
 end
 
 local skullTable = {}
-skullTable[4] = "images/torosaurus.png"
-skullTable[1] = "images/sabertooth.jpg"
-skullTable[2] = "images/trex.jpg"
-skullTable[3] = "images/raptor.jpg"
+skullTable[3] = "tritopSkull.jpg"
+skullTable[2] = "stoothSkull.jpg"
+skullTable[1] = "t-rexSkull.jpg"
+skullTable[4] = "vraptorSkull.jpg"
+
+for i = 1,4 do
+
+-- Create local file for saving data
+local path = system.pathForFile( skullTable[i], system.DocumentsDirectory )
+myFile = io.open( path, "w+b" ) 
+	-- Request remote file and save data to local file
+http.request{
+    url = "http://christijandraper.com/coronaImages/"..skullTable[i] ,
+    sink = ltn12.sink.file(myFile),
+}
+ 
+end
 
 function placePhotos(event)
 
@@ -86,7 +125,7 @@ for i = 1,4 do
 	print ("Placing photos "..xValue.." and "..i)
 	-- Create four droppable photos
 	
-dragImage = display.newImageRect(skullTable[i],_W/2.75,_H/6)
+dragImage = display.newImageRect(skullTable[i],system.DocumentsDirectory,_W/2.8,_H/6)
 	print (skullTable[i])
 	dragImage.x = xValue
 	dragImage.y = yValue*.75
@@ -96,11 +135,12 @@ dragImage = display.newImageRect(skullTable[i],_W/2.75,_H/6)
 	end
 	if (i == 2) then
 		xValue = xValue / 2.75
-		yValue = yValue *1.35
+		yValue = yValue *1.33
 		print("as does this -- Y value " .. yValue)
 	end
 
 	dragImage.j = i
+	dragImage.ID = skullTable[i]
 	dragImage.l = "isDraggable"
 	print("dragimageJ = "..dragImage.j)
 	dragImage:addEventListener( "touch", dragPix )
@@ -113,7 +153,7 @@ end
 if event then
 
 	if event.phase == "began" then
-	xValue = _W/3.75
+	xValue = _W/3.8
 	yValue = yValue / 1.35
 	print ("Began")
 	for k = draggables.numChildren, 1 , -1 do
@@ -139,33 +179,6 @@ setPhotos()
 
 end
 end
---[[
-
-print("X = "..xValue)
-print("Y = "..yValue)
-if event then
-if(event.phase == "began") then
-	for k = 1,4 do
-		print (k) 
-		if(dragImage.j == k) then
-			print ("trial "..k.." Dragimagek = ".. dragImage.j)
-		end
-		if(dragImage.j == k) then
-			print ("Removing" .. k)
-			print (dragImage.x)
-			dragImage.j = nil
-			dragImage.x = nil
-			dragImage.y = nil
-			dragImage:removeEventListener("touch",dragPix)
-			dragImage:removeSelf()
-		end
-		
-	end
-end 
-end ]]  
-	
-
---end
 
 if (dragImage == nil) then
 
@@ -173,43 +186,3 @@ placePhotos()
 
 end   
 button:addEventListener("touch",placePhotos)
-
--- Recognize if dragged photos are in proximity of matching location
-
--- Snap photo into correct position if matches
-
--- Snap back to original position if there is no match
-
-
--- When all photos are aligned, display success message.
-
--- Display buttons that will continue search
-
-
-
--- create object
---[[local myObject = display.newRect( , xValue, yValue )
---myObject:setFillColor( 255 )
-
--- touch listener function
-function myObject:touch( event )
-    if event.phase == "began" then
-
-        self.markX = self.x    -- store x location of object
-        self.markY = self.y    -- store y location of object
-
-    elseif event.phase == "moved" then
-
-        local x = (event.x - event.xStart) + self.markX
-        local y = (event.y - event.yStart) + self.markY
-        
-        self.x, self.y = x, y    -- move object based on calculations above
-    end
-    
-    return true
-end
-
--- make 'myObject' listen for touch events
-myObject:addEventListener( "touch", myObject )
-
-]]--
