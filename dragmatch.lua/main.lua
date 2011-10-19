6c847febd73eed1f52fbddd9a04e3aa35d040c1e
christijan <christijand@mediarain.com>
2011-10-18 22:42:41 -0600

    Had to fix it back to original because of wacky stuff going on in Git

--- dragmatch.lua/main.lua
+++ dragmatch.lua/main.lua
@@ -1,28 +1,16 @@
 _H = display.contentHeight
 _W = display.contentWidth
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 -- Load the relevant LuaSocket modules
 local http = require("socket.http")
 local ltn12 = require("ltn12")
 
 xValue = _W/3.8
 yValue = _H/6.2
-=======
-xValue = _W/3.5
-yValue = _H/5
->>>>>>> Christijan
-=======
-xValue = _W/3.5
-yValue = _H/5
->>>>>>> Christijan
+
 print("Y: "..yValue)
 
 -- Make photos draggable
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 local imageTable = {}
 imageTable[4] = "tritop.jpg"
@@ -47,72 +35,29 @@ end
 
 local button = display.newRect(0,0,44,44)
 button.x = _W/2
-button.y = _H/1.1
-=======
-=======
->>>>>>> Christijan
-local imageTable = {}
-imageTable[4] = "images/triceratops.jpg"
-imageTable[1] = "images/sabertoothLive.jpg"
-imageTable[2] = "images/trexLive.jpg"
-imageTable[3] = "images/raptorLive.jpg"
-
-local skullTable = {}
-skullTable[4] = "images/torosaurus.png"
-skullTable[1] = "images/sabertooth.jpg"
-skullTable[2] = "images/trex.jpg"
-skullTable[3] = "images/raptor.jpg"
-
-local button = display.newRect(0,0,44,44)
-button.x = _W/2
-button.y = _H/3*2
-<<<<<<< HEAD
->>>>>>> Christijan
-=======
->>>>>>> Christijan
+button.y = _H/3*2.7
 
 dropTainer = {}
 for i = 1,4 do
 	-- Create four locations to drop photos
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	local dropContainer = display.newImageRect(imageTable[i],system.DocumentsDirectory,_W/2.75,_H/6)
-=======
-	local dropContainer = display.newImageRect(imageTable[i],_W/2.5,_H/5)
->>>>>>> Christijan
-=======
-	local dropContainer = display.newImageRect(imageTable[i],_W/2.5,_H/5)
->>>>>>> Christijan
+
 	dropContainer.x = xValue
 	dropContainer.y = yValue
 	--dropContainer:setFillColor(0)
 	dropContainer.strokeWidth=1
 	dropContainer:setStrokeColor(250,250,250)
 	if (i == 1) or (i == 3) then
-<<<<<<< HEAD
-<<<<<<< HEAD
 		xValue = xValue *2.8
 	end
 	if (i == 2) then
 		xValue = xValue / 2.8
-=======
-=======
->>>>>>> Christijan
-		xValue = xValue *2.5
-	end
-	if (i == 2) then
-		xValue = xValue / 2.5
-<<<<<<< HEAD
->>>>>>> Christijan
-=======
->>>>>>> Christijan
 		yValue = yValue *2.1
 		
 	end
 	dropTainer[i] = dropContainer
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	dropTainer[i].ID = imageTable[i]
 end
 -- Create four photos to match each of the four titles
@@ -128,6 +73,7 @@ function dragPix( event )
     if event.phase == "began" then
         event.target.markX = event.target.x    -- store x location of object
         event.target.markY = event.target.y    -- store y location of object
+
 		
     elseif event.phase == "moved" then
         local x = (event.x - event.xStart) + event.target.markX
@@ -145,42 +91,12 @@ function dragPix( event )
 			event.target:removeEventListener("touch",dragPix)
 			system.vibrate()
 			display.getCurrentStage():setFocus(nil)
-=======
-=======
->>>>>>> Christijan
-end
--- Create four photos to match each of the four titles
 
-xValue = _W/3.5
-yValue = yValue *2.1
-
-function dragPix( event )
-	local objectNum = event.target.j
-    if event.phase == "began" then
-        event.target.markX = event.target.x    -- store x location of object
-        event.target.markY = event.target.y    -- store y location of object
-    elseif event.phase == "moved" then
-        local x = (event.x - event.xStart) + event.target.markX
-        local y = (event.y - event.yStart) + event.target.markY
-        display.getCurrentStage():setFocus(self)
-		
-        event.target.x, event.target.y = x, y    -- move object based on calculations above
-
--- Snap to correct location		
-		if (x/dropTainer[objectNum].x > .8) and (y/dropTainer[objectNum].y > .8) 
-			and (x/dropTainer[objectNum].x < 1.2) and (y/dropTainer[objectNum].y < 1.2) then
-			event.target.x = dropTainer[objectNum].x
-			event.target.y = dropTainer[objectNum].y
-<<<<<<< HEAD
->>>>>>> Christijan
-=======
->>>>>>> Christijan
 		end
+
 	elseif event.phase == "ended" and event.target.x ~= dropTainer[objectNum].x and event.target.y ~= dropTainer[objectNum].y then
 			event.target.x = event.target.markX
 			event.target.y = event.target.markY
-<<<<<<< HEAD
-<<<<<<< HEAD
 			display.getCurrentStage():setFocus(nil)
 	
     end
@@ -189,9 +105,9 @@ end
 
 local skullTable = {}
 skullTable[3] = "tritopSkull.jpg"
-skullTable[2] = "stoothSkull.jpg"
-skullTable[1] = "t-rexSkull.jpg"
-skullTable[4] = "vraptorSkull.jpg"
+skullTable[1] = "stoothSkull.jpg"
+skullTable[4] = "t-rexSkull.jpg"
+skullTable[2] = "vraptorSkull.jpg"
 
 for i = 1,4 do
 
@@ -268,83 +184,6 @@ setPhotos()
 
 end
 end
-
-if (dragImage == nil) then
-
 placePhotos()
 
-end   
 button:addEventListener("touch",placePhotos)
-=======
-=======
->>>>>>> Christijan
-    end
-  return true
-end
-function placePhotos()
-for i = 1,4 do
-	-- Create four locations to drop photos
-
-	local dragImage = display.newImageRect(skullTable[i],_W/2.5,_H/5)
-	dragImage.x = xValue
-	dragImage.y = yValue*.75
-
-	if (i == 1) or (i == 3) then
-	xValue = xValue *2.5
-end
-if (i == 2) then
-	xValue = xValue / 2.5
-	yValue = yValue *1.35
-end
-
-dragImage.j = i
-dragImage:addEventListener( "touch", dragPix )
-
-end
-end
-placePhotos()
-
--- Recognize if dragged photos are in proximity of matching location
-
--- Snap photo into correct position if matches
-
--- Snap back to original position if there is no match
-
-
--- When all photos are aligned, display success message.
-
--- Display buttons that will continue search
-
-
-
--- create object
---[[local myObject = display.newRect( , xValue, yValue )
---myObject:setFillColor( 255 )
-
--- touch listener function
-function myObject:touch( event )
-    if event.phase == "began" then
-
-        self.markX = self.x    -- store x location of object
-        self.markY = self.y    -- store y location of object
-
-    elseif event.phase == "moved" then
-
-        local x = (event.x - event.xStart) + self.markX
-        local y = (event.y - event.yStart) + self.markY
-        
-        self.x, self.y = x, y    -- move object based on calculations above
-    end
-    
-    return true
-end
-
--- make 'myObject' listen for touch events
-myObject:addEventListener( "touch", myObject )
-
-<<<<<<< HEAD
-]]--
->>>>>>> Christijan
-=======
-]]--
->>>>>>> Christijan
